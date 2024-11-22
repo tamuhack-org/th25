06e74fc4fc722608b2945346902d5d71fdc76038
Eric Li <eric.li.8736@gmail.com>
2024-11-22 00:50:21 -0600

    Switch to black mlh badge

--- src/components/TrustBadge.tsx
+++ src/components/TrustBadge.tsx
@@ -2,7 +2,7 @@ const TrustBadge = () => {
     return (
         <a
             id="mlh-trust-badge"
-            href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white"
+            href="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-black.svg"
             target="_blank"
             rel="noopener noreferrer"
             style={{
