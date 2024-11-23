5e54f3a3763f9520ec34f5644bdbbe7998ed4437
Eric Li <eric.li.8736@gmail.com>
2024-11-23 17:29:21 -0600

    Add initial titling text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,21 @@ import Header from '@/components/Header';
 
 export default function Home() {
     return (
-        <div className="bg-sky-blue px-8">
+        <div className="px-8 max-w-[2000px] mx-auto">
             <Header />
             <main className="mt-24 min-h-screen">
-                <h1>TAMUhack 2025</h1>
+                <h1 className="font-serif text-8xl">TAMUHACK</h1>
+                <h2 className="font-serif text-3xl">
+                    January 25-26, 2025 @ MSC 2300
+                </h2>
+                <div className="mt-6">
+                    <span className="rounded-full border-2 border-black bg-white px-3 py-1 transition-colors hover:bg-[#b1dcfb]">
+                        24 Hours
+                    </span>
+                    <span className="rounded-full border-2 border-black bg-white px-3 py-1 transition-colors hover:bg-[#e3b8dd] ml-4">
+                        Hardware & Software
+                    </span>
+                </div>
             </main>
             <TamuhackFooter />
         </div>
