7bb9f9c32b594ed66d70c92c77c32767c217a933
Justin Le <justinle2006@gmail.com>
2024-11-29 00:48:32 -0600

    added comments to explain code on main branch -- accidentally did it on the wrong branch lol.

--- src/components/faq/Section.tsx
+++ src/components/faq/Section.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import QuestionItem from './Question';
 
 interface SectionProps {
+    // Expects a section title
     title: string;
+    // Expects multiple question items that contains a question and a list of answers
     questions: { question: string; answers: string[] }[];
 }
 
@@ -11,10 +13,12 @@ const SectionItem: React.FC<SectionProps> = ({ title, questions = [] }) => {
         <div className="flex flex-col lg:flex-row">
             <div className="w-full mb-10 lg:mb-0 lg:w-1/4">
                 <h2 id="new-section-title" className="sticky top-8 font-serif text-[#000000] text-4xl">
+                    {/*Add the title to the left 1/4 of screen and make it sticky*/}
                     {title}
                 </h2>
             </div>
             <div className="w-full lg:w-3/4">
+                {/*Add each question to the right 3/4 of screen -- uses QuestionItem for accordion animation*/}
                 {questions.map((item) => (
                     <QuestionItem key={item.question} item={item} />
                 ))}
