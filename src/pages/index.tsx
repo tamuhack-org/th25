1d19148c5bc2a0bdfb9ddad180d06d5308a4ab0a
MDSAJ66 <ajmds66@gmail.com>
2024-12-15 10:45:57 -0600

    Very Unfinished Schedule

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,32 @@ import Header from '@/components/Header';
 import Timer from '@/components/Timer';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from "react";
 import HomeImage from '@/../public/home_image.png';
 
+
+
+interface ScheduleItem {
+    date: Date;
+    description: string;
+    eventName: string;
+    location: string;
+    tags: string[];
+    id: string;
+  };
+
 export default function Home() {
+    const [scheduleItems, setScheduleItems] = useState<ScheduleItem[]>();
+
+    useEffect(() => {
+        const fetchSchedule = async () => {
+          const fetchResult = await fetch('https://team.tamuhack.org/api/hh24-new').then((res) => res.json());
+          setScheduleItems(fetchResult);
+        }
+        fetchSchedule();
+      }, []);
+
+      
     return (
         <div className="mx-auto max-w-[2000px] px-8">
             <Header />
@@ -51,6 +74,47 @@ export default function Home() {
                     />
                 </div>
             </main>
+
+            {/*schedule*/}
+            <div id="schedule" className="flex z-10 w-full max-w-[1200px] h-full">
+            <h2 className="font-serif text-3xl lg:text-8xl">Schedule</h2>
+
+            <div className="flex flex-row gap-2 sm:gap-3 lg:gap-6 w-full">
+            <h3 className="w-full font-serif text-center max-[425px]:text-[12px] text-lg sm:text-2xl lg:text-4xl">Saturday</h3>
+            <div className="flex flex-col gap-1">
+                {scheduleItems?.filter((item) => {
+                    const date = new Date(item.date);
+                    return date.getDay() === 6;
+                }).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()).map((item) => (
+                    <div key={item.id} className="flex flex-col w-full p-4 bg-white border border-black rounded-lg shadow-lg">
+                        <h3 className="text-xl font-bold">{item.eventName}</h3>
+                        <p>{item.description}</p>
+                        <p>{item.location}</p>
+                        <p>{new Date(item.date).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}</p>
+                    </div>
+                
+                ))}
+            </div>
+                
+                </div>
+                <div className="flex flex-row gap-2 sm:gap-3 lg:gap-6 w-full">
+                    <h3 className="w-full font-serif text-center max-[425px]:text-[12px] text-lg sm:text-2xl lg:text-4xl">Sunday</h3>
+                    <div className="flex flex-col gap-1">
+                    {scheduleItems?.filter((item) => {
+                        const date = new Date(item.date);
+                        return date.getDay() === 0;
+                    }).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()).map((item) => (
+                        <div key={item.id} className="flex flex-col w-full p-4 bg-white border border-black rounded-lg shadow-lg">
+                            <h3 className="text-xl font-bold">{item.eventName}</h3>
+                            <p>{item.description}</p>
+                            <p>{item.location}</p>
+                            <p>{new Date(item.date).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}</p>
+                        </div>
+                    ))}
+                    </div>
+                </div>
+            </div>
+
             <TamuhackFooter />
         </div>
     );
