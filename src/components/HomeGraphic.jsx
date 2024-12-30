1311030637977d01e62004137e8b4e79762ada5c
Eric Li <eric.li.8736@gmail.com>
2024-12-30 15:19:33 -0600

    Undo ripple (temporary)

--- src/components/HomeGraphic.jsx
+++ src/components/HomeGraphic.jsx
@@ -1,64 +1,25 @@
-/** eslint-disable **/
 import Image from 'next/image';
+import HomeImage from '@/../public/home_image.png';
 import Guy from '@/../public/guy.png';
-import BigBubble from '@/../public/big_bubble.png';
-import { useEffect } from 'react';
 
 export default function HomeGraphic() {
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const $ = require('jquery');
-            window.$ = window.jQuery = $;
-            require('jquery.ripples');
-
-            if ($.fn.ripples) {
-                $('#home-image-container').ripples({
-                    // Change resolution, perturbance, and dropRadius to change the ripple effect
-                    resolution: 512,
-                    perturbance: 0.02,
-                    dropRadius: 50,
-                });
-                $('#home-image-container').ripples('updateSize');
-            }
-        }
-    }, []);
-
     return (
-        <div id="relative" className="relative">
-            <div>
-                <script
-                    src="http://www.jqueryscript.net/demo/jQuery-Plugin-For-Water-Ripple-Animation-ripples/js/jquery.ripples.js"
-                    defer
-                ></script>
-            </div>
-            <div
-                id="home-image-container"
-                style={{
-                    maxWidth: '800px',
-                    width: '100%',
-                    // Maintain a 4:3 aspect ratio: (600/800)*100% = 75%
-                    paddingBottom: '75%',
-                    background:
-                        'url("/home_image.png") no-repeat center center',
-                    backgroundSize: 'contain',
-                    marginInline: 'auto'
-                }}
-            ></div>
+        <div className="relative">
             <Image
-                src={Guy}
-                alt="Lineart of a man walking"
-                width={64}
-                height={64}
+                src={HomeImage}
+                alt="Reflection of College Station in a puddle"
+                width={800}
+                height={600}
                 placeholder="empty"
-                className="absolute bottom-0 right-[25%] max-w-9 md:max-w-12 lg:max-w-16"
+                className="mx-auto lg:mx-0"
             />
             <Image
-                src={BigBubble}
-                alt=""
+                src={Guy}
+                alt="Just a guy walking"
                 width={64}
                 height={64}
                 placeholder="empty"
-                className="absolute right-[15%] top-7 hidden max-w-12 lg:block"
+                className="absolute bottom-0 right-[25%] max-w-12 lg:max-w-16"
             />
         </div>
     );
