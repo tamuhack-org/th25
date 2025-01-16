5f590855e52f6d65df40d1c4a83a5fedc1a1aeda
ysadamt <adam.teo321@gmail.com>
2025-01-15 19:19:04 -0600

    hide circle schedule tag no match

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -169,11 +169,11 @@ const Schedule: React.FC = () => {
                             <div className="-z-2 absolute bottom-0 left-24 top-0 hidden w-3 bg-black lg:block" />
                             <div className="flex flex-col gap-4">
                                 {!scheduleItems ||
-                                !scheduleItems.filter((item) => {
-                                    const date = new Date(item.date);
-                                    const isDayMatch = date.getDay() === 6;
-                                    return isDayMatch;
-                                }).length ? (
+                                    !scheduleItems.filter((item) => {
+                                        const date = new Date(item.date);
+                                        const isDayMatch = date.getDay() === 6;
+                                        return isDayMatch;
+                                    }).length ? (
                                     <div className="flex h-40 items-center justify-center">
                                         <p className="text-lg text-gray-500">
                                             Coming Soon!
@@ -253,13 +253,12 @@ const Schedule: React.FC = () => {
 
                                                         <div className="flex">
                                                             <div
-                                                                className={`z-10 shrink-0 rounded-full border-4 border-black ${
-                                                                    isEventCurrent(
-                                                                        item,
-                                                                    )
-                                                                        ? '-ml-[29px] h-6 w-6 bg-pink-400'
-                                                                        : '-ml-7 h-5 w-5 bg-blue-100'
-                                                                }`}
+                                                                className={`z-10 shrink-0 rounded-full border-4 border-black ${isEventCurrent(
+                                                                    item,
+                                                                )
+                                                                    ? '-ml-[29px] h-6 w-6 bg-pink-400'
+                                                                    : '-ml-7 h-5 w-5 bg-blue-100'
+                                                                    } ${!tagMatch ? 'hidden' : ''}`}
                                                             />
                                                         </div>
 
@@ -299,15 +298,14 @@ const Schedule: React.FC = () => {
                         <button
                             key={filter}
                             onClick={() => toggleFilter(filter)}
-                            className={`rounded-lg px-2 py-3 text-center text-sm transition-colors ${
-                                activeFilters.includes(
-                                    Object.entries(filterToTagMapping).find(
-                                        (entry) => entry[1] === filter,
-                                    )?.[0] || '',
-                                )
-                                    ? 'bg-black text-white'
-                                    : 'bg-gray-100 text-black hover:bg-gray-200'
-                            }`}
+                            className={`rounded-lg px-2 py-3 text-center text-sm transition-colors ${activeFilters.includes(
+                                Object.entries(filterToTagMapping).find(
+                                    (entry) => entry[1] === filter,
+                                )?.[0] || '',
+                            )
+                                ? 'bg-black text-white'
+                                : 'bg-gray-100 text-black hover:bg-gray-200'
+                                }`}
                         >
                             {filter}
                         </button>
@@ -324,11 +322,11 @@ const Schedule: React.FC = () => {
 
                             <div className="flex flex-col gap-6">
                                 {!scheduleItems ||
-                                !scheduleItems.filter((item) => {
-                                    const date = new Date(item.date);
-                                    const isDayMatch = date.getDay() === 6;
-                                    return isDayMatch;
-                                }).length ? (
+                                    !scheduleItems.filter((item) => {
+                                        const date = new Date(item.date);
+                                        const isDayMatch = date.getDay() === 6;
+                                        return isDayMatch;
+                                    }).length ? (
                                     <div className="flex h-40 items-center justify-center">
                                         <p className="text-lg text-gray-500">
                                             Coming Soon!
@@ -409,13 +407,12 @@ const Schedule: React.FC = () => {
 
                                                         <div className="flex">
                                                             <div
-                                                                className={`z-10 shrink-0 rounded-full border-4 border-black ${
-                                                                    isEventCurrent(
-                                                                        item,
-                                                                    )
-                                                                        ? '-ml-[29px] h-6 w-6 bg-pink-400'
-                                                                        : '-ml-7 h-5 w-5 bg-blue-100'
-                                                                }`}
+                                                                className={`z-10 shrink-0 rounded-full border-4 border-black ${isEventCurrent(
+                                                                    item,
+                                                                )
+                                                                    ? '-ml-[29px] h-6 w-6 bg-pink-400'
+                                                                    : '-ml-7 h-5 w-5 bg-blue-100'
+                                                                    } ${!tagMatch ? 'hidden' : ''}`}
                                                             />
                                                         </div>
 
