4af111124e2fc0cab09f8161417ccb17bab58d14
Justin <justinle2006@gmail.com>
2025-01-17 02:07:59 -0600

    changed from creative + technology to design + development

--- src/components/TamuHackFooter.tsx
+++ src/components/TamuHackFooter.tsx
@@ -182,7 +182,7 @@ const TamuhackFooter = () => {
                         TAMUhack 2025
                     </p>
                     <p className="font-base mb-8 font-poppins text-xl">
-                        Website brought to you by the TAMUhack Creative + Technology Teams
+                        Website brought to you by the TAMUhack Design + Development Teams
                     </p>
                 </div>
                 <div className="flex flex-grow flex-wrap justify-start gap-12 lg:justify-end">
