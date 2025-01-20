b4c02f098c6232bb5f205e9e6b03344eff347583
ysadamt <adam.teo321@gmail.com>
2025-01-19 20:22:34 -0600

    sponsor challenges

--- src/components/prizes/Prizes.tsx
+++ src/components/prizes/Prizes.tsx
@@ -53,8 +53,28 @@ const softwarePrizesWithSponsors: Prize[] = [
     },
     {
         title: 'American Airlines Challenge',
+        description: 'Create a solution for the airline industry, be it passenger experience, employee experience, or operational efficiencies',
+        prize: '1st Place: AAdvantage 75k miles\n2nd Place: AAdvantage 50k miles\n3rd Place: AAdvantage 25k miles',
+    },
+    {
+        title: 'Arize AI Challenge',
         description: 'More details coming soon!',
-        prize: 'Prize: TBA',
+        prize: 'Prize: $100 Amazon Gift Card',
+    },
+    {
+        title: 'ARM Challenge',
+        description: 'Develop a solution to improve disaster preparedness, response, or post-event analysis, leveraging the power of the Arm GitHub Copilot Extension. Examples include water usage monitoring, drought prediction dashboards, communication apps for response teams, or damage assessments. Submissions using Arm technology, such as a Raspberry Pi or Arm-based server, will be given preference.\n\nNote: This challenge can include both software and hardware components.',
+        prize: '1st Place: Sony Noise-cancelling Wireless Headphones\n2nd Place: Arm Developer Program Swag Bundle',
+    },
+    {
+        title: 'AWS Challenge',
+        description: 'Create an innovative solution that leverages AI/ML to address a real-world challenge in one of the following industries:\n\nHealthcare: Use AI to develop a solution to improve patient care, streamline administrative processes, or advance medical research\n\nSustainability: Use AI to develop a solution to enhance energy efficiency, reduce waste, or optimize resource management.\n\nEducation: Use AI to develop a solution to personalize learning experiences, improve accessibility, or enhance skill development.\n\nBonus points for utilizing AWS services such as Amazon Bedrock, Amazon Rekognition, Amazon Polly, or AWS IoT for a broader impact.',
+        prize: '1st Place: $75 Amazon Gift Card\n2nd Place: $50 Amazon Gift Card\n3rd Place: $25 Amazon Gift Card',
+    },
+    {
+        title: 'Capital One Challenge',
+        description: "Best Financial Hack - Capital One's track challenges hackers to develop the Best Financial Hack. Participants will harness their creativity and technical skills to design and innovate tools that will help with anything related to finance.",
+        prize: 'Prize: $250 Amazon Gift Card (per team member)',
     },
     {
         title: 'Toyota Challenge',
