f406b8ed1c36f533a0d7dfff247a06264f1a98b3
Ju5t1nL3 <justinle2006@gmail.com>
2025-01-27 17:40:02 -0600

    added ripple effect

--- src/components/HomeGraphic.jsx
+++ src/components/HomeGraphic.jsx
@@ -1,15 +1,54 @@
 import Image from 'next/image';
 import HomeImage from '@/../public/home_image.png';
 import Guy from '@/../public/guy.png';
+import { useEffect } from 'react';
 
 export default function HomeGraphic() {
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const $ = require('jquery');
+            window.$ = window.jQuery = $;
+            require('jquery.ripples');
+
+            if ($.fn.ripples) {
+                $('#home-image-container').ripples({
+                    // Change resolution, perturbance, and dropRadius to change the ripple effect
+                    resolution: 512,
+                    perturbance: 0.02,
+                    dropRadius: 50,
+                });
+                $('#home-image-container').ripples('updateSize');
+            }
+        }
+    }, []);
+
     return (
         <div className="relative">
+            <div>
+                <script
+                    src="http://www.jqueryscript.net/demo/jQuery-Plugin-For-Water-Ripple-Animation-ripples/js/jquery.ripples.js"
+                    defer
+                ></script>
+            </div>
+            <div
+                id="home-image-container"
+                alt="Reflection of College Station in a puddle"
+                style={{
+                    maxWidth: '800px',
+                    width: '100%',
+                    // Maintain a 4:3 aspect ratio: (600/800)*100% = 75%
+                    paddingBottom: '75%',
+                    background:
+                        'url("/home_image.png") no-repeat center center',
+                    backgroundSize: 'contain',
+                }}
+                className="hidden lg:flex"
+            ></div>
             <Image
                 src={HomeImage}
                 alt="Reflection of College Station in a puddle"
                 placeholder="empty"
-                className="mx-auto lg:mx-0"
+                className="flex lg:hidden mx-auto lg:mx-0"
                 quality={50}
                 priority
             />
